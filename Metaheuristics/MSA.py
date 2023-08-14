222d85c74c725e0fd3841407fc9c110ddb271c00
javiers1999 <56318901+javiers1999@users.noreply.github.com>
2023-08-14 19:03:33 -0400

    Metodo SORT

--- Metaheuristics/MSA.py
+++ Metaheuristics/MSA.py
@@ -11,12 +11,6 @@ def iterarMSA(maxIter: int, t: int, population: np.array, bestSolution) -> np.ar
     Implementation of https://link.springer.com/article/10.1007/s12293-016-0212-3
 
     TODO: SORT POPULATION BY FITNESS BEFORE ITERATING!
-
-    """Metodo que se me ocurrio para hacer el sort (REVISAR)"""
-     for t in range(MaxGen):   #MaxGen es el numero de generacion, que se le entrega como parametro
-        x_fitness = [fitness(xi) for xi in x]
-        sorted_indices = np.argsort(x_fitness)
-        x = x[sorted_indices]
     
     TODO: Check if the constants defined below are supposed to be actual constants,
            or we got something wrong. Also, set them to more adequate values.
@@ -30,6 +24,14 @@ def iterarMSA(maxIter: int, t: int, population: np.array, bestSolution) -> np.ar
         (beta - 1) * gamma(beta - 1) * math.sin(math.pi * (beta - 1) / 2)
     ) / math.pi * s**beta
     length = len(population)
+    
+    """Metodo que se me ocurrio para hacer el sort (REVISAR)"""
+     for t in range(MaxGen):   #MaxGen es el numero de generacion, que se le entrega como parametro
+        x_fitness = [fitness(xi) for xi in x]
+        sorted_indices = np.argsort(x_fitness)
+        x = x[sorted_indices]
+    """REVISAR"""
+    
     for i, candidate in enumerate(population[:length//2]):
         # Levy flight
         population[i] = candidate + (
